593e15aa5e46dc8e1005f6a126eda518b87d1d36
popugang <d3xrow@gmail.com>
2018-09-28 14:44:19 +0300

    upd HMR

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
-import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshNormalMaterial, Mesh, Color } from 'three';
 
-const scene = new Scene();
-const camera = new PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
+let camera, scene, renderer;
+let geometry, material, mesh;
 
-const renderer = new WebGLRenderer()
+function init() {
 
-renderer.setSize(window.width, window.innerHeight)
+  camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+  camera.position.z = 1;
 
-document.body.appendChild(renderer.domElement)
+  scene = new Scene();
+
+  geometry = new BoxGeometry( 0.4, 0.6, 0.2 );
+  material = new MeshNormalMaterial();
+
+  mesh = new Mesh( geometry, material );
+  scene.add( mesh );
+
+  renderer = new WebGLRenderer( { antialias: true } );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+  requestAnimationFrame( animate );
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+
+  renderer.render( scene, camera );
+
+}
+
+init();
+animate();
+
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
+  document.querySelector('canvas').remove();
+  module.hot.dispose(() => {
+    document.body.appendChild( renderer.domElement );
+  })
 }
